8e2035b419bf61fb8b9ad99ae9de42b97123d38c
CodeByDani <danialabedini1380@gmail.com>
2024-08-10 11:29:39 +0330

    HiddenVilla Compeleted

--- Models/HotelRoomDTO.cs
+++ Models/HotelRoomDTO.cs
@@ -1,24 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Models
 {
     public class HotelRoomDTO
     {
         public int Id { get; set; }
-        [Required(ErrorMessage = "Please enter room name")]
+        [Required(ErrorMessage = "اسم اتاق الزامیست")]
         public string Name { get; set; }
-        [Required(ErrorMessage = "Please enter Occupancy")]
+        [Required(ErrorMessage = "ظرفیت الزامیست")]
         public int Occupancy { get; set; }
-        [Range(1, 3000, ErrorMessage = "Regular rate must be between 1 and 3000")]
+        [Range(1000, 3000, ErrorMessage = "بین 1000 تا 3000 تومان باید باشد")]
         public double RegularRate { get; set; }
         public string Details { get; set; }
         public string SqFt { get; set; }
-
         public double TotalDays { get; set; }
         public double TotalAmount { get; set; }
 
